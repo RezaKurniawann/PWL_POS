d00480a797aaa241255779f1a542ade04c38ab19
rezakurniawann <reza.kurniawan0110@gmail.com>
2024-11-06 13:12:42 +0700

    Jobsheet 10 (Restful API) - Praktikum 1 (Membuat Restful API Register)

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -3,67 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-// class UserModel extends Model
-// {
-//     use HasFactory;
-
-//     protected $table = 'm_user'; // mendefinisikan nama tabel yang digunakan oleh model ini
-//     protected $primaryKey = 'user_id'; //mendefinisikan primary key dari tabel yang digunakan
-//     /**
-//      * The attributes that are mass assignable.
-//      * 
-//      * @var array
-//      */
-//     // protected $fillable = ['level_id', 'username', 'nama', 'password'];
-//     protected $fillable = ['username', 'password', 'nama', 'level_id', 'created_at', 'updated_at'];
-
-//     protected $hidden = ['password'];
-//     protected $casts = ['password' => 'hashed'];
-
-//     public function level(): BelongsTo {
-//         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
-//     }
-// };
-
-class UserModel extends Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
     use HasFactory;
 
-    protected $table = 'm_user'; // mendefinisikan nama tabel yang digunakan oleh model ini
-    protected $primaryKey = 'user_id'; //mendefinisikan primary key dari tabel yang digunakan
-    /**
-     * The attributes that are mass assignable.
-     * 
-     * @var array
-     */
-    // protected $fillable = ['level_id', 'username', 'nama', 'password'];
-    protected $fillable = ['username', 'password', 'nama', 'level_id', 'created_at', 'updated_at'];
-
-    protected $hidden = ['password'];
-    protected $casts = ['password' => 'hashed'];
-
-    public function level(): BelongsTo
-    {
-        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
-    }
+    protected $table = 'm_user'; // Corrected property name
+    protected $primaryKey = 'user_id';
     
-    //  Mendapatkan nama role
-    public function getRoleName(): string
+    protected $fillable = [
+        'username', 
+        'nama', 
+        'password', 
+        'level_id' // Removed 'password_confirmation'
+    ];
+
+    public function getJWTIdentifier()
     {
-        return $this->level->level_nama;
+        return $this->getKey();
     }
 
-    public function hasRole($role): bool
+    public function getJWTCustomClaims()
     {
-        return $this->level->level_kode == $role;
+        return [];
     }
-
-    public function getRole()
-    {
-        return $this->level->level_kode;
-    }
-};
+}
